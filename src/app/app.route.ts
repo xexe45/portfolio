8658511d0e48dff23500929a4a2044becdaa0010
Cesar Lachira <lachiracesar@gmail.com>
2018-01-19 11:44:13 -0500

    Modificacion Navegacion

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,10 +2,10 @@ import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent,ItemComponent,PortafolioComponent} from './components/index.paginas';
 const app_routes: Routes = [
 
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'item', component: ItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes,{useHash:true});
