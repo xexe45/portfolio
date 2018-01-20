bdd9967e9fcad7784fae4ee3d3be4230b0181cab
Cesar Lachira <lachiracesar@gmail.com>
2018-01-20 18:12:14 -0500

    Servicio de Firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,14 +4,30 @@ import { HttpClient } from '@angular/common/http';
 export class InformacionService {
   info:any = {};
   cargada:boolean = false;
-  constructor(private http:HttpClient) {
+  cargadaSobre:boolean = false;
+  equipo:any[] = [];
 
-    this.http.get("assets/data/info.pagina.json")
-      .subscribe((data) => {
-        this.info = data;
-        this.cargada = true;
-        console.log(data);
-      })
+  constructor(private http:HttpClient) {
+    this.cargaInfo();
+    this.cargaSobreNosotros();
+
+   }
+
+   public cargaInfo(){
+     this.http.get("assets/data/info.pagina.json")
+       .subscribe((data) => {
+         this.info = data;
+         this.cargada = true;
+       })
+   }
+
+   public cargaSobreNosotros(){
+     this.http.get("https://heroesapp-1ab0c.firebaseio.com/equipo.json")
+       .subscribe((data:any) => {
+         console.log(data);
+         this.cargadaSobre = true;
+         this.equipo = data;
+       })
    }
 
 }
